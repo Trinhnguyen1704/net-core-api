72dc53e6b921cf06d36dde180e9e443d3439aea8
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-07-22 14:14:56 +0700

    change model todoItem  to book

---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_core_api.Models
+{
+    public class Book
+    {
+        public int  Id {get;set;}
+        public string Title {get; set;}
+        public string Author {get;set ;}
+        public bool IsAvalable {get;set;}
+        public string Description {get;set;}
+    }
+}
