ff59e81d95562307cb1c8060135b0194edf53ec6
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-07-27 11:05:53 +0700

    config dependence with Category table

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,16 +1,26 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace net_core_api.Models
 {
-    public class Book
+    [Table("Book")]
+    public partial class Book
     {
+        [Key]
         public int  Id {get;set;}
+        [Required]
         public string Title {get; set;}
         public string Author {get;set ;}
+        [Required]
         public bool IsAvailable {get;set;}
         public string Description {get;set;}
+        public int? CategoryId {get;set;}
+        [ForeignKey("CategoryId")]
+
+        public virtual Category CategoryIdNavigation {get; set;}
     }
 }
