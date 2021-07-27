070a445f354ed2af1ddbb3b8c6538e13fbcaf1dc
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-07-27 17:10:03 +0700

    config one to many ralationship

--- Models/BookContext.cs
+++ Models/BookContext.cs
@@ -14,5 +14,14 @@ namespace net_core_api.Models
         }
         public DbSet<Book> Books {get; set;}
         public DbSet<Category> Categories {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        // configures one-to-many relationship
+            modelBuilder.Entity<Category>()
+            .HasMany(c => c.Books)
+            .WithOne(b => b.CategoryIdNavigation)
+            .HasForeignKey(b => b.CategoryId);
+        }
     }
 }
