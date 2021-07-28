883dfd0f07509791074f5c9a2cfd78fa54f63df6
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-07-28 09:58:49 +0700

    prepare for the student management project

--- Startup.cs
+++ Startup.cs
@@ -12,8 +12,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
-using net_core_api.Models;
-using net_core_api.Repositories;
+// using net_core_api.Models;
+// using net_core_api.Repositories;
 using System.Text.Json.Serialization;
 
 namespace net_core_api
@@ -31,8 +31,8 @@ namespace net_core_api
         public void ConfigureServices(IServiceCollection services)
         {
             //register repositories
-            services.AddScoped<IBookRepository, BookRepository>();
-            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            // services.AddScoped<IBookRepository, BookRepository>();
+            // services.AddScoped<ICategoryRepository, CategoryRepository>();
             //register controllers
             services.AddControllers().AddNewtonsoftJson(options =>
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
@@ -41,7 +41,7 @@ namespace net_core_api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "net_core_api", Version = "v1" });
             });
-            services.AddDbContext<BookContext>(options => options.UseSqlServer(@"Data Source=ADMIN; Initial Catalog=bookstore;" + "User ID=ADMIN; Integrated Security=True;"));
+            // services.AddDbContext<BookContext>(options => options.UseSqlServer(@"Data Source=ADMIN; Initial Catalog=bookstore;" + "User ID=ADMIN; Integrated Security=True;"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
