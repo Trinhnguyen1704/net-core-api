073b9e1a1bbd9131720fe633c387a5e3fb1c8450
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-07-29 04:31:26 +0700

    add many to many relationship migration

---
+++ Migrations/20210728212748_ManyToManyRelationship.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace net_core_api.Migrations
+{
+    public partial class ManyToManyRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Student_Class_ClassId",
+                table: "Student");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Student_ClassId",
+                table: "Student");
+
+            migrationBuilder.DropColumn(
+                name: "ClassId",
+                table: "Student");
+
+            migrationBuilder.CreateTable(
+                name: "ClassStudent",
+                columns: table => new
+                {
+                    ClassesClassId = table.Column<int>(type: "int", nullable: false),
+                    StudentsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClassStudent", x => new { x.ClassesClassId, x.StudentsId });
+                    table.ForeignKey(
+                        name: "FK_ClassStudent_Class_ClassesClassId",
+                        column: x => x.ClassesClassId,
+                        principalTable: "Class",
+                        principalColumn: "ClassId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ClassStudent_Student_StudentsId",
+                        column: x => x.StudentsId,
+                        principalTable: "Student",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClassStudent_StudentsId",
+                table: "ClassStudent",
+                column: "StudentsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClassStudent");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ClassId",
+                table: "Student",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Student_ClassId",
+                table: "Student",
+                column: "ClassId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Student_Class_ClassId",
+                table: "Student",
+                column: "ClassId",
+                principalTable: "Class",
+                principalColumn: "ClassId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
