fa058870d04b0c0ea118cdea36f620b46c561b59
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-07-28 10:38:29 +0700

    create student repository with basic method

---
+++ Repositories/IStudentRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using net_core_api.Models;
+
+namespace net_core_api.Repositories
+{
+    public interface IStudentRepository
+    {
+        Task<IEnumerable<Student>> GetStudents();
+        Task<Student> GetStudent(int id);
+        Task<Student> AddStudent(Student student);
+        Task Update(Student student);
+        Task Delete(int id);
+    }
+}
---
+++ Repositories/StudentRepository.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using net_core_api.Models;
+
+namespace net_core_api.Repositories
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly DatabaseContext _context;
+        public StudentRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+        public async Task<Student> AddStudent(Student student)
+        {
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+            return student;
+        }
+
+        public async Task Delete(int id)
+        {
+            var studentToDelete = await GetStudent(id);
+            _context.Students.Remove(studentToDelete);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Student> GetStudent(int id)
+        {
+            return await _context.Students.Include(x => x.ClassNavigation)
+            .FirstOrDefaultAsync(student => student.Id == id);
+        }
+
+        public async Task<IEnumerable<Student>> GetStudents()
+        {
+            return await _context.Students.Include(x => x.ClassNavigation).ToListAsync();
+        }
+
+        public async Task Update(Student student)
+        {
+            var studentToUpdate = await GetStudent(student.Id);
+
+            studentToUpdate.Name = student.Name;
+            studentToUpdate.ClassId = student.ClassId;
+            studentToUpdate.DateOfBirth = student.DateOfBirth;
+            studentToUpdate.AverageMark = student.AverageMark;
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
