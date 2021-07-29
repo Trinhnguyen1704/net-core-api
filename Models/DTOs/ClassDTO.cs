70b592234b128d6fdb2461f83533d411670380b7
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-07-29 14:55:10 +0700

    create Models DTO

---
+++ Models/DTOs/ClassDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_core_api.Models.DTOs
+{
+    public class ClassDTO 
+    {
+        public int ClassId {get; set;}
+        public string ClassName {get; set;}
+    }
+}
