bef05f8be87e132b3e00d71af17c0dd7e0e97dcc
Nguyễn Thị Thùy Trinh <51841079+Trinhnguyen1704@users.noreply.github.com>
2021-07-30 17:09:23 +0700

    add class statistic dto

---
+++ Models/DTOs/ClassSatisticDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_core_api.Models.DTOs
+{
+    public class ClassSatisticDTO
+    {
+        public string ClassName {get; set;}
+        public long TotalStudents {get; set;}
+    }
+}
